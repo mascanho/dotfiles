536682b0dcce8c305d00f0b561547beb231a47b6
mascanho <mascanho@sapo.pt>
2023-04-15 00:29:20 +0100

    nvim with Lazyvim plugin manager

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,33 +1,33 @@
 return {
   {
-    'akinsho/toggleterm.nvim',
+    "akinsho/toggleterm.nvim",
     version = "*",
 
     config = function()
-    require("toggleterm").setup {
-      size = 10,
-      open_mapping = [[<c-\>]],
-      hide_numbers = true,
-      shade_terminals = false,
-      insert_mappings = true,
-      persist_size = true,
-      direction = "float",
-      close_on_exit = true,
-      shell = vim.o.shell,
-      autochdir = true,
-      highlights = {
-        NormalFloat = {
-          link = "Normal",
+      require("toggleterm").setup({
+        size = 10,
+        open_mapping = [[<c-\>]],
+        hide_numbers = true,
+        shade_terminals = false,
+        insert_mappings = true,
+        persist_size = true,
+        direction = "float",
+        close_on_exit = true,
+        shell = vim.o.shell,
+        autochdir = true,
+        highlights = {
+          NormalFloat = {
+            link = "Normal",
+          },
+          FloatBorder = {
+            link = "FloatBorder",
+          },
         },
-        FloatBorder = {
-          link = "FloatBorder",
+        float_opts = {
+          border = "rounded",
+          winblend = 0,
         },
-      },
-      float_opts = {
-        border = "rounded",
-        winblend = 0,
-      },
-    }
-    end
-  }
+      })
+    end,
+  },
 }
--- nvim/nvim/lua/plugins/lualine.lua
+++ nvim/nvim/lua/plugins/lualine.lua
@@ -1,23 +1,21 @@
 return {
   {
-    "nvim-lualine/lualine.nvim",
+    'nvim-lualine/lualine.nvim',
     dependencies = {
-      "nvim-tree/nvim-web-devicons",
-      opt = true,
+      'nvim-tree/nvim-web-devicons', opt = true
     },
 
     opts = {
       options = {
         icons_enabled = true,
-        theme = "auto",
+        theme = 'auto',
         component_separators = "",
         section_separators = {
-          left = "",
-          right = "",
+          left = '', right = ''
         },
         disabled_filetypes = {
           statusline = {
-            "alpha",
+            'alpha'
           },
           winbar = {},
         },
@@ -28,71 +26,63 @@ return {
           statusline = 1000,
           tabline = 1000,
           winbar = 1000,
-        },
+        }
       },
       sections = {
         lualine_a = {
           {
-            "mode",
-            separator = {
-              left = "",
-              right = "",
+            'mode', separator = {
+              left = '', right = ''
             },
           },
         },
         lualine_b = {
           {
-            "branch",
-            icon = "󰘬",
+            'branch', icon = '󰘬',
           },
           {
-            "diff",
+            'diff',
             symbols = {
-              added = " ",
-              modified = " ",
-              removed = " ",
+              added = ' ', modified = ' ', removed = ' '
             },
-          },
+          }
         },
         lualine_c = {
-          "filename",
+          'filename'
         },
         lualine_x = {
-          "encoding",
-          "fileformat",
+          'encoding', 'fileformat',
         },
         lualine_y = {
-          "diagnostics",
+          'diagnostics'
         },
         lualine_z = {
-          "progress",
+          'progress',
           {
-            "location",
-            separator = {
-              right = "",
-            },
-            left_padding = 1,
+            'location', separator = {
+              right = ''
+            }, left_padding = 1
           },
-        },
+        }
       },
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
         lualine_c = {
-          "filename",
+          'filename'
         },
         lualine_x = {
-          "location",
+          'location'
         },
         lualine_y = {},
-        lualine_z = {},
+        lualine_z = {}
       },
       tabline = {},
       winbar = {},
       inactive_winbar = {},
       extensions = {
-        "nvim-tree",
-      },
-    },
-  },
+        "nvim-tree"
+      }
+    }
+  }
 }
